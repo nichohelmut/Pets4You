8e371f6a066f518bdc1a90930d44414cf8bbc24e
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-13 15:50:37 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,50 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+namespace :customers do
+  get 'dashboard', to: "pages#dashboard", as: "dashboard"
+  resources :requests, only: [:new, :create, :update, :destroy]
 end
+
+
+
+
+namespace :owners do
+  get 'dashboard', to: "pages#dashboard", as: "dashboard"
+  resources :pets, only: [:new, :create, :update, :destroy]
+end
+
+resources :pets, only: [:index, :show]
+
+
+
+
+
+  # namespace :owners do
+  #   resources :user do
+  #     resources :pet, only: [:index, :show, :new, :create]
+  #     end
+  #     resources :request, only: [:index, :show]
+
+  # end
+
+
+  # namespace :customers do
+  #   resources :user do
+  #     resources :pet, only: [:index, :show]
+  #       resources :request, only: [:index, :show, :new, :create]
+  #     end
+  #   end
+
+end
+
+
+
+
+  # namespace :customer do
+  #   resources :user, only:
+  # end
+
+  # resources :request, only: [:index, :show, :new, :create]
+# end
